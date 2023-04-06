0b3ec27ed45f9909634dace150dac0426b5fb961
Bolot-u <bolot.moldoev.ur@gmail.com>
2023-04-06 15:10:05 +0200

    hw7

--- posts/views.py
+++ posts/views.py
@@ -1,17 +1,30 @@
 from django.shortcuts import render, redirect
 from posts.models import Post, Review
 from posts.forms import PostCreateForm, ReviewCreateForm
+from posts.constants import PAGINATION_LIMIT
 def main_page_view(request):
     if request.method == 'GET':
         return render(request, 'layouts/index.html')
 def products_view(request):
     if request.method == 'GET':
+        posts = Post.objects.all().order_by('-created_date')
+        search = request.GET.get('search')
+        page = int(request.GET.get('page', 1))
 
-        posts = Post.objects.all()
-
+        if search:
+            posts = posts.filter(title__icontains=search) | posts.filter(description__icontains=search)
+        max_page = posts.__len__() / PAGINATION_LIMIT
+        posts = posts[PAGINATION_LIMIT * (page-1):PAGINATION_LIMIT * page]
+        if round(max_page) < max_page:
+            max_page = round(max_page) + 1
+        else:
+            max_page = round(max_page)
+        print(max_page)
         context = {
             'posts': posts,
-            'user': request.user
+            'user': request.user,
+            'pages': range(1, max_page + 1)
+
         }
         return render(request, 'products/products.html',context=context)
 
